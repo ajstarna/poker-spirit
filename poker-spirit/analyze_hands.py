7b1d7c3bc09c000cfb2a770c6a2f8d4665ba209c
Adam St Arnaud <ajstarna@ualberta.ca>
2020-04-30 22:24:53 -0600

    main files

---
+++ poker-spirit/analyze_hands.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python3
+'''
+This script reads and processes hands written by PokerStars to their text file
+'''
+
+import re
+from collections import defaultdict
+
+CASH_GAME = True
+HAND_START_PATTERN = re.compile("PokerStars.*Hand #(\d+)")
+if CASH_GAME:
+    MONEY = ".+"
+else:
+    MONEY = "/w+"
+    
+SEAT_PATTERN = re.compile(f"Seat (\d): (.+) \(({MONEY}) in chips\)")
+
+SMALL_PATTERN = re.compile(f"(.+): posts small blind ({MONEY})")
+BIG_PATTERN = re.compile(f"(.+): posts big blind ({MONEY})")
+
+FLOP_PATTERN = re.compile(f"\*\*\* FLOP \*\*\* \[(\w+) (\w+) (\w+)\]")
+
+CALL_PATTERN = re.compile(f"(.+): calls ({MONEY})")
+RAISE_PATTERN = re.compile(f"(.+): raises ({MONEY}) to ({MONEY})")
+FOLD_PATTERN = re.compile(f"(.+): folds")
+CHECK_PATTERN = re.compile(f"(.+): checks")
+
+TURN_PATTERN = re.compile(f"\*\*\* TURN \*\*\* \[(\w+) (\w+) (\w+)\] \[(\w+)\]")
+
+
+class PlayerHand:
+    __slots__ = 'name', 'sb', 'bb', 'vpip', 'pfr', 'first_to_raise_pre_flop', \
+        '_3_bet', 'calls_3_bet_opp', 'folds_3_bet_opp', \
+        '_4_bet', '_5_bet', \
+        'folds_to_3_bet', 'calls_3_bet', \
+        'c_bet', 'checks_c_bet_opp',  \
+        'folds_to_c_bet', 'calls_c_bet', 'raises_c_bet', \
+    
+    def __init__(self, name):
+        self.name = name
+        self.sb = None
+        self.bb = None
+        self.vpip = False
+        self.pfr = False
+        self.first_to_raise_pre_flop = False
+        self._3_bet = False
+        self.calls_3_bet_opp = False
+        self.folds_3_bet_opp = False                
+        self._4_bet = False
+        self._5_bet = False
+        self.folds_to_3_bet = False
+        self.calls_3_bet = False        
+        self.c_bet = False
+        self.checks_c_bet_opp = False
+        self.folds_to_c_bet = False
+        self.calls_c_bet = False
+        self.raises_c_bet = False
+        
+        
+def set_up_hand(hand_lines, players):
+    # print('in set up hand')
+    for i, line in enumerate(hand_lines):
+        # print(f'line = {line}')
+        seat_match = SEAT_PATTERN.match(line)
+        if seat_match:
+            player_name = seat_match.group(2)
+            players[player_name] = PlayerHand(name=player_name)
+            continue
+        
+        small_match = SMALL_PATTERN.match(line)
+        if small_match:
+            player_name = small_match.group(1)
+            sb = small_match.group(2)
+            # print(f'small blind player is {player_name} in for {sb}')
+            players[player_name].sb = sb
+            continue
+        
+        big_match = BIG_PATTERN.match(line)
+        if big_match:
+            player_name = big_match.group(1)
+            bb = big_match.group(2)            
+            # print(f'big blind player is {player_name} in for {bb}')
+            players[player_name].bb = bb
+            # once we found the big blind we break
+            break
+    return i
+
+    
+def analyze_pre_flop(hand_lines, pre_flop_index, players):
+    num_raises = 0 
+    for j, line in enumerate(hand_lines[pre_flop_index:]):
+        flop_match = FLOP_PATTERN.match(line)
+        if flop_match:
+            break
+
+        call_match = CALL_PATTERN.match(line)
+        if call_match:
+            player_name = call_match.group(1)                        
+            # print(f'player {player_name} called!')
+            player = players[player_name]
+            if num_raises == 1:
+                player.calls_3_bet_opp = True            
+            elif num_raises == 2:
+                player.calls_3_bet = True
+            player.vpip = True
+
+        raise_match = RAISE_PATTERN.match(line)
+        if raise_match:
+            player_name = raise_match.group(1)                        
+            # print(f'player {player_name} raised!')
+            player = players[player_name]
+            player.vpip = True
+            player.pfr = True
+            
+            if num_raises == 0:
+                player.first_to_raise_pre_flop = player
+            elif num_raises == 1:
+                # if already a raise, and we raising again, it's a 3 bet
+                player._3_bet = True
+            elif num_raises == 2:
+                player._4_bet = True                
+            elif num_raises == 3:
+                player._5_bet = True
+            elif num_raises == 4:
+                player._5_bet = True
+            num_raises += 1
+
+        fold_match = FOLD_PATTERN.match(line)
+        if fold_match:
+            player_name = fold_match.group(1)                        
+            # print(f'player {player_name} folds!')
+            player = players[player_name]            
+            if num_raises == 1:
+                player.folds_3_bet_opp = True                                
+            elif num_raises == 2:
+                player.folds_to_3_bet = True                                
+
+    return j
+        
+def analyze_flop(hand_lines, flop_index, players):
+    active_c_bet = False
+    num_raises = 0
+    
+    for i, line in enumerate(hand_lines[flop_index:]):
+        turn_match = TURN_PATTERN.match(line)
+        if turn_match:
+            return i
+
+        check_match = CHECK_PATTERN.match(line)
+        if check_match:
+            player_name = check_match.group(1)                        
+            # print(f'player {player_name} checks!')
+            player = players[player_name]            
+            if player.pfr:
+                player.checks_c_bet_opp = True
+        
+        call_match = CALL_PATTERN.match(line)
+        if call_match:
+            player_name = call_match.group(1)                        
+            # print(f'player {player_name} called!')
+            player = players[player_name]            
+            if active_c_bet:
+                player.calls_c_bet = True
+                # player.seen_c_bet += 1
+            
+
+        raise_match = RAISE_PATTERN.match(line)
+        if raise_match:
+            player_name = raise_match.group(1)
+            player = players[player_name]
+            if active_c_bet:
+                player.raises_c_bet = True
+                # player['seen_c_bet'] += 1
+            
+            # print(f'player {player} raised!')
+
+            if num_raises == 0 and player.pfr:               
+                # print('ACTIVE C BET')
+                active_c_bet = True
+                player.c_bet = True
+            else:
+                active_c_bet = False
+                
+            num_raises += 1
+
+        fold_match = FOLD_PATTERN.match(line)
+        if fold_match:
+            player_name = fold_match.group(1)                        
+            # print(f'player {player_name} folds!')
+            player = players[player_name]
+            if active_c_bet:
+                player.folds_to_c_bet = True
+                # player.seen_c_bet = True
+
+def process_single_hand(hand_lines, game_stats):
+    ''' given all the lines of the text file that correspond to a given hand,
+    analyze what happens
+    '''
+    # print('in process_hand_lines')
+    # print(hand_lines)
+    if hand_lines is None:
+        return
+
+    hand_stats = {}
+    players = {}
+    last_set_up_index = set_up_hand(hand_lines, players)
+    
+    # after the big blind is the line "*** HOLE CARDS ***",
+    # so we skip past that
+    pre_flop_index = last_set_up_index + 2
+
+    flop_index = analyze_pre_flop(hand_lines, pre_flop_index, players)
+
+    analyze_flop(hand_lines, flop_index, players)
+    assign_stats_from_hand(players, game_stats)
+    
+
+def assign_stats_from_hand(players, game_stats):
+    game_stats['current_players']  = set(players.keys()) # for knowing who is still at the table
+    
+    for player_name, player in players.items():
+        if player_name not in game_stats:
+            game_stats[player_name] = defaultdict(int)
+        player_stats = game_stats[player_name]
+        player_stats['hands_played'] += 1
+        if player.vpip:
+            player_stats['vpip'] += 1
+        if player.pfr:
+            player_stats['pfr'] += 1
+            
+        if player._3_bet:
+            player_stats['3_bet'] += 1
+            player_stats['3_bet_opp'] += 1            
+        if player.folds_3_bet_opp:
+            player_stats['folds_3_bet_opp'] += 1            
+            player_stats['3_bet_opp'] += 1
+        if player.calls_3_bet_opp:
+            player_stats['calls_3_bet_opp'] += 1            
+            player_stats['3_bet_opp'] += 1
+            
+        if player._5_bet:
+            player_stats['5_bet'] += 1
+
+        if player.folds_to_3_bet:
+            player_stats['folds_to_3_bet'] += 1
+            player_stats['seen_3_bet'] += 1            
+        elif player.calls_3_bet:
+            player_stats['calls_3_bet'] += 1
+            player_stats['seen_3_bet'] += 1            
+        elif player._4_bet:
+            player_stats['4_bet'] += 1
+            player_stats['seen_3_bet'] += 1            
+
+        if player.c_bet:
+            player_stats['c_bet'] += 1
+            player_stats['c_bet_opp'] += 1            
+        elif player.checks_c_bet_opp:
+            player_stats['checks_c_bet_opp'] += 1
+            player_stats['c_bet_opp'] += 1
+
+        if player.folds_to_c_bet:
+            player_stats['folds_to_c_bet'] += 1
+            player_stats['seen_c_bet'] += 1            
+        elif player.calls_c_bet:
+            player_stats['calls_c_bet'] += 1
+            player_stats['seen_c_bet'] += 1            
+        elif player.raises_c_bet:
+            player_stats['raises_c_bet'] += 1
+            player_stats['seen_c_bet'] += 1            
+
+            
+            
+def print_stats(game_stats):
+    for player in sorted(game_stats['current_players'], key=lambda x: x.lower() ):
+        stats = game_stats[player]
+        
+        print(f'Player: {player}')
+        print(f'Hands played = {stats["hands_played"]}')
+        
+        print(f'VPIP = {round(100*stats["vpip"]/stats["hands_played"], 1)}')
+        print(f'PFR = {round(100*stats["pfr"]/stats["hands_played"], 1)}')
+
+        if stats['3_bet_opp'] > 0:        
+            print(f'3-Bet = {round(100*stats["3_bet"]/stats["3_bet_opp"], 1)}')
+            
+        print(f'4-Bet = {round(100*stats["4_bet"]/stats["hands_played"], 1)}')
+        
+        if stats['seen_3_bet'] > 0:
+            print(f'Folds to 3-Bet = {round(100*stats["folds_to_3_bet"]/stats["seen_3_bet"], 1)}')
+            print(f'Calls 3-Bet = {round(100*stats["calls_3_bet"]/stats["seen_3_bet"], 1)}')
+
+        if stats['seen_c_bet'] > 0:
+            print(f'Folds to C-Bet = {round(100*stats["folds_to_c_bet"]/stats["seen_c_bet"], 1)}')
+            print(f'Calls C-Bet = {round(100*stats["calls_c_bet"]/stats["seen_c_bet"], 1)}')
+            print(f'Raises C-Bet = {round(100*stats["raises_c_bet"]/stats["seen_c_bet"], 1)}')                    
+
+        if stats['c_bet_opp'] > 0:
+            print(f'C-Bets = {round(100*stats["c_bet"]/stats["c_bet_opp"], 1)}')
+            print(f'Checks C-Bet Opportunity = {round(100*stats["checks_c_bet_opp"]/stats["c_bet_opp"], 1)}')
+            
+        print()
+        
+def run_file(path_to_file):
+    print(f"opening file = {path_to_file}")
+    game_stats = {}
+    with open(path_to_file, 'r') as file:
+        lines = file.read().split('\n')
+        print(len(lines))
+        hand_lines = None
+
+        for line in lines:
+            hand_match = HAND_START_PATTERN.match(line)
+            if hand_match:
+                # print(f'match on line: {line}')
+                # print(f'group: {hand_match.group(0)}')
+                process_single_hand(hand_lines, game_stats)
+                hand_lines = []
+                
+            if hand_lines is not None:
+                hand_lines.append(line)
+
+    print("DONE RUN FILE")
+    return game_stats
+
+if __name__ == "__main__":
+    game_stats = run_file(PATH_TO_FILE)
+    print_stats(game_stats)
---
+++ poker-spirit/app.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+from functools import partial
+
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from tkinter import ttk
+
+import analyze_hands
+
+class App:
+    def __init__(self):
+        self.filename = FILE_NAME
+        self.player_windows = {}
+
+    def get_truncated_name(self, filename):
+        print(filename)
+        if len(filename) > 75:
+            truncated = '....' + filename[-75:]
+        else:
+            truncated = filename
+        return truncated
+    
+    def select_file(self):
+        filename =  askopenfilename(initialdir = PATH_TO_FILES,
+                                    title = "Select file"
+        )
+
+        if filename == '':
+            return
+        else:
+            self.filename = filename
+
+        truncated = self.get_truncated_name(filename)
+        self.text_var.set(f"filename = {truncated}")
+
+
+    def insert_stats_into_text(self, text, stats):
+        text.delete(0.0, tk.END) # clear what is there
+
+        hands = stats['hands_played']
+        
+        text.insert(tk.END,
+                    f'VPIP = {stats["vpip"]}/{hands} = {round(100*stats["vpip"]/hands, 1)}\n'
+        )
+        text.insert(tk.END,
+                    f'PFR = {stats["pfr"]}/{hands} = {round(100*stats["pfr"]/hands, 1)}\n'
+        )
+        if stats["3_bet_opp"] > 0:
+            text.insert(tk.END,
+                        f'3-Bet = {stats["3_bet"]}/{stats["3_bet_opp"]} = {round(100*stats["3_bet"]/stats["3_bet_opp"], 1)}\n'
+            )
+        else:
+            text.insert(tk.END,
+                        f'3-Bet = {stats["3_bet"]}/{stats["3_bet_opp"]} = 0\n'
+            )
+            
+        text.insert(tk.END,
+                    f'4-Bet = {stats["4_bet"]}/{hands} = {round(100*stats["4_bet"]/hands, 1)}'
+        )
+
+        
+    def read_file(self):
+        if self.filename is None:
+            return
+        else:
+            game_stats = analyze_hands.run_file(self.filename)
+            current_players = game_stats['current_players']
+            for player in sorted(current_players, key=lambda x: x.lower()):
+                stats = game_stats[player]
+                if player in self.player_windows:
+                    print(f'player {player} already current!')
+                    window = self.player_windows[player]
+                    print(window.winfo_children())
+                    text = window.winfo_children()[0]
+                    print()
+                else:
+                    # a new player has entered the table
+                    window = tk.Tk()
+                    window.geometry("165x60")
+                    window.title(player)
+                    text = tk.Text(window)
+
+                    
+                self.insert_stats_into_text(text, stats)
+                text.pack()
+                self.player_windows[player] = window
+                    
+            # don't want to have windows lingering around for players who
+            # are no longer at the table
+            for player in self.player_windows:
+                if player not in current_players:
+                    del self.player_windows[player]
+            
+    def quit(self):
+        self.window.destroy()
+        for window in self.player_windows.values():
+            window.destroy()
+            
+    def main(self):
+        self.window = tk.Tk()
+        self.window.geometry("575x100")
+        
+        self.window.title( "Poker Spirit")
+
+        button_select = ttk.Button(self.window,
+                                   text="Select File",
+                                   command=self.select_file,
+        ).pack()
+
+        self.text_var = tk.StringVar()
+        truncated = self.get_truncated_name(self.filename)        
+        self.text_var.set(f"filename = {truncated}")
+        file_label = ttk.Label(self.window, textvariable=self.text_var).pack() #text=f"filename = {self.filename}")
+
+        button_run = ttk.Button(self.window,
+                                text="Read and Analyze File",
+                                command=self.read_file
+        ).pack()
+        button_quit = ttk.Button(self.window,
+                                 text="Quit",
+                                 command=self.quit
+        ).pack()
+
+        self.window.mainloop()
+
+
+app = App()
+app.main()
