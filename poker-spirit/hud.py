8cfd806b7253fb199be8b79c2eee388c9f8835e6
Michael Feist <mdfeist@ualberta.ca>
2020-06-03 20:44:36 -0600

    Add single window view. Need to add ability to show both windows.

--- poker-spirit/hud.py
+++ poker-spirit/hud.py
@@ -12,14 +12,33 @@ from db_management import PokerStarsDBManager
 from dotenv import load_dotenv
 
 
+class PlayerStatsWindow:
+    def merge_current_and_career_stats(self, game, career_stats):
+        '''
+        If we have the career stats and the current game stats for a given player,
+        we want to display the career stats INCLUDING the current game, so return a new
+        dict with the summed values.
+        '''
+        new_stats = {}
+        for player in game.current_players:
+            stats = game.game_stats[player]
+            new_stats[player] = defaultdict(int)
+            for stat, game_val in stats.items():
+                new_stats[player][stat] = game_val + career_stats[player][stat]
+        return new_stats
 
-class PlayerWindowsManager:
+    def populate(self, game, career_stats_by_player):
+        pass
+
+    def destroy():
+        PokerStarsGameIO
+
+class PlayerWindowsManager(PlayerStatsWindow):
     # making this its own class so that we can use this code in the live tracker gui as well
     def __init__(self):
         self.player_windows = {}
         self.player_career_windows = {}        
 
-        
     def destroy(self):
         for window in self.player_windows.values():
             window.destroy()
@@ -49,21 +68,6 @@ class PlayerWindowsManager:
         text.insert(tk.END,
                     GameIO._4_bet_str(game_stats, player_name) + "\n"                
         )
-
-
-    def merge_current_and_career_stats(self, game, career_stats):
-        '''
-        If we have the career stats and the current game stats for a given player,
-        we want to display the career stats INCLUDING the current game, so return a new
-        dict with the summed values.
-        '''
-        new_stats = {}
-        for player in game.current_players:
-            stats = game.game_stats[player]
-            new_stats[player] = defaultdict(int)
-            for stat, game_val in stats.items():
-                new_stats[player][stat] = game_val + career_stats[player][stat]
-        return new_stats
     
     def populate(self, game, career_stats_by_player):
         new_stats = self.merge_current_and_career_stats(game, career_stats_by_player)
@@ -104,14 +108,55 @@ class PlayerWindowsManager:
         for player_name in list(self.player_windows.keys()):
             if player_name not in game.current_players:
                 del self.player_windows[player_name]
-                del self.player_career_windows[player_name]                
+                del self.player_career_windows[player_name]    
+
+class AllPlayerWindowManager(PlayerStatsWindow):
+    # making this its own class so that we can use this code in the live tracker gui as well
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("640x512")
+        self.window.title("Stats")
+        self.tree = ttk.Treeview(self.window, columns=('player', 'hands', 'vpip', 'pfr'), show='headings')   
+        self.tree.heading('player', text="Player")
+        self.tree.heading('hands', text="Hands Played")
+        self.tree.heading('vpip', text="VPIP")
+        self.tree.heading('pfr', text="PFR")
+        
+    def destroy(self):
+        self.window.destroy()
+        
+    def insert_stats_into_text(self, game):
+        game_stats = game.game_stats
+
+        # clear treeview
+        self.tree.delete(*self.tree.get_children())
+        for player_name in game.current_players:
+            stats = game_stats[player_name]
+            hands_played = stats['hands_played']
+            vpip = 0.0
+            pfr = 0.0
+
+            if hands_played > 0:
+                vpip = round(100*stats["vpip"]/hands_played, 1)
+                pfr = round(100*stats["pfr"]/hands_played, 1)
+
+            self.tree.insert("", "end", values=(player_name, hands_played, vpip, pfr))
+
+
+    def populate(self, game, career_stats_by_player):
+        new_stats = self.merge_current_and_career_stats(game, career_stats_by_player)
+
+        self.insert_stats_into_text(game)
+        self.tree.pack(side=tk.TOP,fill=tk.X)
+   
         
 class App:
     def __init__(self):
         self.filename = None
         self.db_manager = PokerStarsDBManager()
         self.career_stats_by_player = {}
-        self.pwm = PlayerWindowsManager()
+        self.pwm = None
+        self.one_window = None
         load_dotenv()
         # get env vars from .env file
         self.path_to_hands = os.getenv("PATH_TO_HANDS", ".")
@@ -144,6 +189,12 @@ class App:
         if self.filename is None:
             return
         else:
+            if self.pwm is None:
+                if self.one_window.get():
+                    self.pwm = AllPlayerWindowManager()
+                else:
+                    self.pwm = PlayerWindowsManager()
+
             self.game = PokerStarsGameIO(self.filename)
             self.game.run_file()
             self.career_stats_by_player = self.db_manager.load_player_history(self.game, self.career_stats_by_player)
@@ -172,6 +223,9 @@ class App:
         truncated = self.get_truncated_name(self.filename)        
         self.text_var.set(f"filename = {truncated}")
         file_label = ttk.Label(self.window, textvariable=self.text_var).pack() #text=f"filename = {self.filename}")
+
+        self.one_window = tk.IntVar()
+        ttk.Checkbutton(self.window, text="All in One Window", variable=self.one_window).pack()
 
         button_run = ttk.Button(self.window,
                                 text="Read and Analyze File",
